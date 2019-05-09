88a6a2acd6ca4fa9819e24ee53e5c4cadec8a562
matteo_zeggiotti <matteo.zeggiotti@gmail.com>
2019-05-09 18:03:19 +0200

    potentiometer read

--- Projects/Elaborato01/src/main.cpp
+++ Projects/Elaborato01/src/main.cpp
@@ -1,10 +1,24 @@
-#include <mbed.h>
+#include "mbed.h"
+
+AnalogIn input(A0);
 
 int main() {
 
   // put your setup code here, to run once:
+  uint16_t samples[1024];
+
+  for(int i=0; i<1024;i++){
+    samples[i] = input.read_u16();
+    wait(0.001f);
+  }
+
+  printf("Result: \n");
+  for(int i=0; i<1024; i++){
+    printf("%d, 0x%04X\n",i,samples[i]);
+  }
 
   while(1) {
     // put your main code here, to run repeatedly:
+    
   }
 }
